f793e4dbac1a4aa03346f76843fc7462e9a91265
poorya0991 <poorya.0991@gmail.com>
2021-01-09 11:51:50 +0330

    sherkat 20 dey

--- script.js
+++ script.js
@@ -73,25 +73,38 @@
 
 let detections = [];
 
-const video1 = document.getElementById('video')
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+
 Promise.all([
-     faceapi.nets.faceLandmark68Net.loadFromUri('/face/models'),
-     faceapi.nets.tinyFaceDetector.loadFromUri('/face/models')
+     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+     faceapi.nets.tinyFaceDetector.loadFromUri('/models')
 ]).then(()=>startVideo())
 
 
 
 var myCanvas;
 function setup() {
-  myCanvas = createCanvas(video1.width, video1.height);
+  blendModes = [ 
+    MULTIPLY, 
+    BLEND, 
+    ADD, 
+    DARKEST, 
+    LIGHTEST, 
+    DIFFERENCE, 
+    EXCLUSION, 
+    OVERLAY, 
+    HARD_LIGHT, 
+    SOFT_LIGHT, 
+    DODGE, 
+    BURN 
+  ] 
+  
+  index = 0; 
+  currBlendMode = blendModes[index]; 
+  myCanvas = createCanvas(640, 480);
   myCanvas.parent('container');
   myCanvas.position(0, 0);
-  blendMode(MULTIPLY); 
-  filter(BLUR, 3); 
+  capture = createCapture(VIDEO);
+  filter(BLUR, 1); 
   
 }
 
@@ -101,17 +114,19 @@ function setup() {
  
  function draw() {
   
+
+  image(capture, capture.x, capture.y, capture.width, capture.height);
   
   if (detections.length > 0) {
     clear()
+    blendMode(currBlendMode); 
       const lab = detections[0].landmarks.getMouth();
 
-      console.log(detections)
       beginShape();
   
   
   noStroke()
-  fill('rgba(204, 0, 0, 0.7)');
+  fill('rgba(204, 0, 0, 0.4)');
 
   curveVertex(lab[0].x,lab[0].y);
   //  circle(lab[0].x,lab[0].y, 10); 
@@ -128,32 +143,36 @@ function setup() {
 }
 
 
-function startVideo() {
-    navigator.getUserMedia(
-      { video: {} },
-      stream => video1.srcObject = stream,
-      err => console.error(err)
-    )
-  }
 
-    video1.addEventListener('play', () => {
-     const displaySize = { width: video.width, height: video.height }
- 
-    setInterval(async () => {
-    const detectionsWithLandmarks = await faceapi.detectAllFaces(video1,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-    faceapi.matchDimensions(myCanvas, displaySize)
-    const resizedDetections = faceapi.resizeResults(detectionsWithLandmarks, displaySize)
+async function startVideo(){
+
+  const displaySize = { width: capture.width, height: capture.height }
+  const detectionsWithLandmarks = await faceapi.detectAllFaces(capture.elt,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+  faceapi.matchDimensions(myCanvas, displaySize)
+  const resizedDetections = faceapi.resizeResults(detectionsWithLandmarks, displaySize)
   console.log('1')
-  
-  
- detections = resizedDetections
-     
-    
-    }, 100)
-  })
+
+
+detections = resizedDetections
+
+}
 
 
 
+var myVar = setInterval(startVideo, 1000);
 
 
-  
+function changeBlendMode() { 
+  if (index < blendModes.length - 1) 
+    index++; 
+  else
+    index = 0; 
+  currBlendMode = blendModes[index]; 
+  console.log(currBlendMode)
+} 
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    changeBlendMode()
+  } 
+}
